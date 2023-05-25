7ea13c5bf49f438c0fdac14ea0b77d7f2a329371
SafiyaSamreen <92724659+SafiyaSamreen@users.noreply.github.com>
2023-05-25 16:37:20 +0530

    Add files via upload

---
+++ Exception_Handling/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Task1
+{
+    internal static class Program
+    {
+        static char ReturnFirstCharacter(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentNullException(nameof(str));
+
+            char firstChar = str[0];
+            return firstChar;
+        }
+        private static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter the string");
+            input = Console.ReadLine();
+            Console.WriteLine("The first character of string is : {0} ", ReturnFirstCharacter(input));
+        }
+    }
+}
---
+++ Exception_Handling/Task2/NumberParser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        private static void validateString(string stringValue)
+        {
+            if (stringValue == null)
+                throw new ArgumentNullException(nameof(stringValue));
+
+            stringValue = stringValue.Trim();
+
+            if (stringValue == "")
+                throw new FormatException(nameof(stringValue));
+
+            for (int i = 0; i < stringValue.Length; i++)
+            {
+                if (!char.IsDigit(stringValue[i]))
+                {
+                    if (i == 0 && (stringValue[i] == '+' || stringValue[i] == '-'))
+                        continue;
+
+                    throw new FormatException(nameof(stringValue));
+                }
+            }
+
+            if ((stringValue.Length >= 10 && stringValue != "-2147483648"))
+            {
+                if (stringValue != "2147483647")
+                    throw new OverflowException(nameof(stringValue));
+            }
+        }
+        public int Parse(string stringValue)
+        {
+            validateString(stringValue);
+            stringValue = stringValue.Trim();
+
+            var number = 0;
+            bool isNegative = false;
+
+            if (stringValue[0] == '-')
+                isNegative = true;
+
+            if (stringValue != "0" && stringValue != "-0" && stringValue != "+0")
+            {
+                int i = 0;
+
+                while (stringValue[i] == '-' || stringValue[i] == '+' || stringValue[i] == '0')
+                    i++;
+
+               for(int j = i; j < stringValue.Length; j++)
+                {
+                    number = number * 10 + (stringValue[j] - 48);
+                }
+
+               if (isNegative)
+                    number = number * -1;
+            }
+            return number;
+        }
+    }
+}
